9472d7166c79a0b121b6857f93f83c3751a3439b
AROLLAND1 <alexandre.rolland85@ynov.com>
2024-01-02 02:58:02 +0100

    Add TODO

--- lib/Geolocator/services/get_country_code.dart
+++ lib/Geolocator/services/get_country_code.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+// TODO: Add permission check on ios and android
 Future<String> getCountryFromLatLng(double latitude, double longitude) async {
   final apiUrl =
       'https://nominatim.openstreetmap.org/reverse?format=json&lat=$latitude&lon=$longitude&zoom=18&addressdetails=1';
